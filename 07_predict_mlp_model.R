d26eb60d5e71c190c426887303f6b478da085ef5
Ubuntu <ubuntu@ip-172-31-43-180.eu-west-1.compute.internal>
2018-01-04 13:55:20 +0000

    pred values

--- 07_predict_mlp_model.R
+++ 07_predict_mlp_model.R
@@ -1,25 +1,37 @@
 library(mxnet)
 
-model <- mx.model.load("mlp", 35)
-preds <- predict(model, 
-                 X = mnist_validate,
-                 ctx = mx.cpu())
+#model <- mx.model.load("mlp", 35)
 
 
 mnist_validate$reset()
 mnist_validate$iter.next()
+
 val <- mnist_validate$value()
 val$data <- as.array(val$data)
 val$label  <- as.array(val$label)
 
-#plot digit
+#preds <- predict(model, 
+#                 X = val$data,
+#                 ctx = mx.cpu())
+#ypred = max.col(t(as.array(preds)))
+#head(ypred,20)
 
-image(t(apply(matrix(val$data[, 1], nrow = 28, byrow = TRUE),2,rev)),  col = grey(seq(0,1,length.out = 256)))
-title(main =sprintf("Record number=[%s]. Label = %s", 1, val$label[1]))
+preds <- predict(model, 
+                 X = mnist_validate,
+                 ctx = mx.cpu())
+ypred = max.col(t(as.array(preds)))-1
+head(ypred,20)
+
+head(val$label,20)
 
 
-for (t in 100:120) {
+t=1
+image(t(apply(matrix(val$data[, t], nrow = 28, byrow = TRUE),2,rev)),  col = grey(seq(0,1,length.out = 256)))
+title(main =sprintf("Record number=[%s]. Label = %s. Pred = %s", t, val$label[t],ypred[t]))
+
+
+for (t in 1:25) {
   image(t(apply(matrix(val$data[, t], nrow = 28, byrow = TRUE),2,rev)),  col = grey(seq(0,1,length.out = 256)))
-  title(main =sprintf("Record number=[%s]. Label = %s", t, val$label[t]))
+  title(main =sprintf("Record number=[%s]. Label = %s. Pred = %s", t, val$label[t],ypred[t]))
 }
 
